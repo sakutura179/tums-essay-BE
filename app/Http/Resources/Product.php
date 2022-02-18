a174ee48d0f0146b881f48a92cd1696fa4800f8e
khanhs <khanhnguyen3916@gmail.com>
2022-02-18 12:17:09 +0700

    API controller and resource file

---
+++ app/Http/Resources/Product.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Product extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        /*
+            Tra ve 1 JSON dang
+            {
+                "data": [
+                    {
+                        'id': 1,
+                        ...
+                    },
+                    ...
+                ]
+            }
+
+            Nen o FE phai chuyen doi lai thanh 1 mang
+        */
+        return [
+            'product_id' => $this->product_id,
+            'cate_id' => $this->cate_id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'price' => $this->price,
+            'size' => $this->sizes,
+            'desc' => $this->desc,
+            'image' => $this->images,
+            'quantity' => $this->quantity,
+        ];
+    }
+}
